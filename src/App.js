7eaf21fb9efbc62a7fc3ea44410a0bd2e37dc360
marioxe301 <mariogflores301@hotmail.com>
2019-03-23 14:01:11 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./App.css";
+import SampleComponent from "./components/SampleComponent";
+import { Provider as ReduxProvider } from "react-redux";
+import  {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import configureStore from "./modules/store";
+
+const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
+
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact="/" component={SampleComponent}/>
+      <Route component={SampleComponent}/>
+    </Switch>
+  </Router>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <ReduxProvider store={reduxStore}>
+        <div className="App">
+          {routing}
+        </div>
+      </ReduxProvider>
+    );
+  }
+}
+
+export default App;
