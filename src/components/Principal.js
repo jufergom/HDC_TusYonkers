2072f4247e51739f9aca72dab9afc1171a54b024
jufergom <jufergom@hotmail.com>
2019-07-01 21:57:34 -0600

    retired bicicletas from navbar

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -3,36 +3,13 @@ import  {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
 import './principal.css';
 import Logo from '../images/Logo1.png';
 import Slideshow from "./Slideshow";
+import NavBar from './NavBar';
 
 class Principal extends Component {
     render() {
         return(
             <div>
-                <header>
-                    <div className="contenedor">
-                        <div className="logo">
-                            <img src={Logo} className="logoImage" alt="Logo_Name"/>
-                        </div>
-                    </div>
-                </header>
-                <div className="navegacion">
-                    <nav>
-                        <ul className="NavBar">
-                        <li><Link to="/">TusYonkers</Link></li>
-                        
-                        <li><a>Repuestos</a>
-                            <ul>
-                            <li><Link to="/ListCars">Carros</Link></li>
-                            <li><Link to="/ListMoto">Motocicletas</Link></li>
-                            <li><Link to="/ListBike">Bicicletas</Link></li>
-                            </ul>
-                        </li>
-                        <li><Link to="/">Yonkers</Link></li>
-                        <li><Link to="/Contact">Contactanos</Link></li>
-                        <li><Link to="/Login">Iniciar Sesion</Link></li>
-                        </ul>
-                    </nav>
-                </div>
+                <NavBar />
                 <div>
                     <Slideshow/>
                 </div>
